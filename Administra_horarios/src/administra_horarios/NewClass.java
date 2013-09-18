60b22dbcbbf8558043c8ee4ff61fd9ed6ce194a8
José Daniel Alfaro Bolaños <flickter55@gmail.com>
2013-09-18 13:11:00 -0600

    algo

---
+++ Administra_horarios/src/administra_horarios/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package administra_horarios;
+
+/**
+ *
+ * @author José Daniel
+ */
+public class NewClass {
+    
+}
